7b2250808476b24e3ca3da22b08c62e537327d05
Georgie <227040015@qq.com>
2022-01-13 01:58:21 +0000

    删除文件 src/services/admin/upload.js

--- src/services/admin/upload.js
+++
@@ -1,6 +0,0 @@
-import {request, METHOD} from '@/utils/request'
-import {UPLOAD_FILE} from '@/services/api';
-
-export async function uploadFile(data) {
-    return request(UPLOAD_FILE, METHOD.POST, data);
-}
