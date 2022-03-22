b14da807a025c6a4d38b96fbaf8e2d49f67a620a
Georgie <227040015@qq.com>
2022-03-22 04:59:09 +0000

    update src/services/api.js.

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ const API_PROXY_PREFIX = 'http://127.0.0.1:6363/api'
 const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX
 // const BASE_URL = process.env.VUE_APP_API_BASE_URL
 module.exports = {
+    BASEURL: BASE_URL,
     LOGIN: `${BASE_URL}/admin/login`,
     INFO: `${BASE_URL}/admin/info`,
     ROUTES: `${BASE_URL}/admin/routes`,
